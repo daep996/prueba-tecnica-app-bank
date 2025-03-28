34fdbc38f4d919f7b75d9b001a7f880407eaf47a
daep996 <daep996@gmail.com>
2025-03-28 05:59:13 -0500

    BACK add cors

--- BACK/src/server.ts
+++ BACK/src/server.ts
@@ -2,9 +2,12 @@ import express from 'express';
 import authRoutes from './routes/auth.routes';
 import accountRoutes from './routes/accounts.routes';
 import transactionRoutes from './routes/transactions.routes';
+import cors from 'cors';
 
 const app = express();
 app.use(express.json());
+
+app.use(cors())
 
 app.use('/auth', authRoutes);
 app.use('/accounts', accountRoutes);
